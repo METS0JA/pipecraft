ee7d53f5b11976b59141cab499c86ced55cc92e1
Suvaline Vana <suvaline.vana@gmail.com>
2021-03-08 12:19:13 +0200

    git cleanup

--- src/pipecraft-core/image_development/dada2_install.R
+++
@@ -1,4 +0,0 @@
-install.packages("Rcpp" ,repos = "http://cran.us.r-project.org")
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager" ,repos = "http://cran.us.r-project.org")
-BiocManager::install("dada2", version = "3.10")
