5d250d05882fcd1bf66691013be3cd5885c28af7
Martin Metsoja <suvaline.vana@gmail.com>
2026-04-10 18:53:41 +0300

    latest metamate

--- src/store/index.js
+++ src/store/index.js
@@ -1818,7 +1818,7 @@ export default new Vuex.Store({
             tooltip:
               "metaMATE (metabarcoding Multiple Abundance Threshold Evaluator) analyses a set of amplicons derived through metabarcoding of a mitochondrial coding locus to determine putative NUMT and other erroneous sequences",
             scriptName: "metamate.sh",
-            imageName: "pipecraft/metamate:0.5.3",
+            imageName: "pipecraft/metamate:0.5.4-pc1.2.0",
             serviceName: "metaMATE",
             selected: false,
             showExtra: false,
